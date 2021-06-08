84a89bce227fc1ad529e1113ee51c5b0e97aa6f5
Mark Jones <mark@taocode.com>
2021-06-08 15:44:05 -0400

    Added support for pin-to-bottom, attop and atbottom extensions.

--- src/index.svelte
+++ src/index.svelte
@@ -1,24 +1,33 @@
 <script>
-  import { createEventDispatcher } from "svelte";
+  import { createEventDispatcher, onMount } from "svelte";
   import validate from "./validation";
 
   export let duration = "300ms";
   export let offset = 0;
   export let tolerance = 0;
+  export let bottom = false;
 
   let headerClass = "pin";
   let lastHeaderClass = "pin";
   let y = 0;
   let lastY = 0;
+  let prefix = "";
+  let attop: boolean = true;
+  let atbottom: boolean = false;
+  let win: Window;
 
   const dispatch = createEventDispatcher();
 
+  onMount(() => {
+    win = window;
+  });
+
   function deriveClass(y = 0, scrolled = 0) {
-    if (y < offset) return "pin";
+    if (y < offset) return `${prefix}pin`;
     if (!scrolled || Math.abs(scrolled) < tolerance) return headerClass;
     const dir = scrolled < 0 ? "down" : "up";
-    if (dir === "up") return "pin";
-    if (dir === "down") return "unpin";
+    if (dir === "up") return `${prefix}pin`;
+    if (dir === "down") return `${prefix}unpin`;
     return headerClass;
   }
 
@@ -36,8 +45,10 @@
   $: {
     validate({ duration, offset, tolerance });
     headerClass = updateClass(y);
+    attop = y <= 2;
+    atbottom = win && (win.innerHeight + win.pageYOffset) >= document.body.offsetHeight - 2;
     if (headerClass !== lastHeaderClass) {
-      dispatch(headerClass);
+      dispatch(headerClass ? "unpin" : "pin");
     }
     lastHeaderClass = headerClass;
   }
@@ -56,9 +67,24 @@
   .unpin {
     transform: translateY(-100%);
   }
+  .atbottom.svelte-headroom--unpin {
+    transform: translateY(0);
+  }
+  .bottom,
+  .bottom.atbottom.svelte-headroom--unpin {
+    top: auto;
+    bottom: 0;
+    width: auto;
+    right: 0;
+    transform: translateY(0);
+  }
+  .svelte-headroom--unpin.bottom,
+  .bottom.attop {
+    transform: translateY(100%);
+  }
 </style>
 
 <svelte:window bind:scrollY={y} />
-<div use:action class={headerClass}>
+<div use:action class={headerClass} class:bottom class:attop class:atbottom>
   <slot />
 </div>
