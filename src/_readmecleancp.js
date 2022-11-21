5e4650211b0a213772c17ad81270d3d51ef29d32
taocode <mark@taocode.com>
2022-11-21 16:31:35 -0500

    fix(build): _README.md path updated

--- src/_readmecleancp.js
+++ src/_readmecleancp.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 
 try {
-  const readme = fs.readFileSync(path.resolve(process.cwd()+'/src','README.md'))
+  const readme = fs.readFileSync(path.resolve(process.cwd()+'/src','_README.md'))
   const cleaned = readme.toString().replaceAll(/ \{#.*\}/g,'')
   console.log('Overwriting /README.md with clean copy')
   fs.writeFileSync(path.resolve(process.cwd(),'README.md'),cleaned)
